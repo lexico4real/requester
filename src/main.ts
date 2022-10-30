87c66828989d82f4639651fd7051b40ee12a85f6
Oluyinka Abubakar <lexico4real@gmail.com>
2022-10-31 00:03:10 +0100

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { TransformInterceptor } from '../config/interceptor/transform.interceptor';
+import ClusterConfig from 'config/system/cluster';
+import CorsConfig from 'config/system/cors';
+import SwaggerConfig from 'config/swagger/config';
+
+async function bootstrap() {
+  const cluster = new ClusterConfig();
+  const cors = new CorsConfig();
+  const doc = new SwaggerConfig();
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+  await cors.set(app);
+  app.setGlobalPrefix('/api/v1');
+  await doc.set(app);
+  app.useGlobalInterceptors(new TransformInterceptor());
+  app.enableShutdownHooks();
+  await cluster.set(app);
+}
+bootstrap();
